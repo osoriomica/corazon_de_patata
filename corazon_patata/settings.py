e255e02bf00707655d619ec891a6f4bd7788cd61
Michelle Osorio <osoriomica@gmail.com>
2025-05-06 18:19:06 +0100

    Set secret key variable

--- corazon_patata/settings.py
+++ corazon_patata/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-70g*4$lce!)(3x&jj4=!t2@#g9tiregt!g=xbebb1zxez*+ivr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
