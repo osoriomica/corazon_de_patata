3907384e22916e25b4bc416f79a35813cfb7d4ff
Michelle Osorio <osoriomica@gmail.com>
2025-05-10 13:04:09 +0100

    Register Bookmark model in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, Comment, Rating
+from .models import Recipe, Comment, Rating, Bookmark
 
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Comment)
 admin.site.register(Rating)
+admin.site.register(Bookmark)
