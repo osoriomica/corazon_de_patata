52c935ca55d4b32f216751e772e918a9dfe3da63
Michelle Osorio <osoriomica@gmail.com>
2025-05-13 19:25:18 +0100

    Make recipe_detail.view, add default image

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipesList.as_view(), name='home'),  # URL for the recipe list view
-#     path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),  # URL for the recipe detail view
-#     path('recipe/add/', views.RecipeCreateView.as_view(), name='recipe_add'),  # URL for adding a new recipe
-#     path('recipe/<int:pk>/edit/', views.RecipeUpdateView.as_view(), name='recipe_edit'),  # URL for editing a recipe
-#     path('recipe/<int:pk>/delete/', views.RecipeDeleteView.as_view(), name='recipe_delete'),  # URL for deleting a recipe
+    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),  # URL for the recipe detail view
+    # path('bookmarks/', views.BookmarksView.as_view(), name='bookmarks'),
+    # path('bookmark/<int:recipe_id>/', views.bookmark_recipe, name='bookmark_recipe'),
+    # path('unbookmark/<int:recipe_id>/', views.unbookmark_recipe, name='unbookmark_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -13,3 +13,22 @@ class RecipesList(generic.ListView):
     # def get_queryset(self):
     #     """Return the list of recipes, ordered by created_at."""
     #     return Recipe.objects.all().order_by('-created_at')
+
+def recipe_detail(request, slug):
+    """
+    View to display a single recipe.
+
+    Context:
+    ``recipe``
+        An instance of :model `Recipe`.
+     **Template:**
+    ``recipes/recipe_detail.html``
+    """
+
+    queryset = Recipe.objects.filter(status='published')
+    recipe = get_object_or_404(queryset, slug=slug)
+    return render(
+        request, 
+        'recipes/recipe_detail.html', 
+        {'recipe': recipe}
+    )
