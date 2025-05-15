7af4b26722247cf38c8a49adb3f21d90849bcb6a
Michelle Osorio <osoriomica@gmail.com>
2025-05-16 00:03:15 +0100

    Delete env.py from GitHub repo

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-os.environ.setdefault(
-    'DATABASE_URL', 'postgresql://neondb_owner:npg_pLbvn31JDAPw@ep-crimson-pine-a2gza9gx.eu-central-1.aws.neon.tech/deed_grant_denim_567342'
-    )
-os.environ.setdefault(
-    'SECRET_KEY', '075157880793Boob4B34R')
